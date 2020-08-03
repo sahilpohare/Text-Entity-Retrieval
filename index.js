8cb4f56a65d03e7149cd8f07c437f7329018a77e
GC54 <sahilpohare>
2020-08-03 11:17:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { parseTextWithTemplateBase, ExtractArraysAndStrings } = require('./parser');
+const express = require('express')();
+const bodyParser = require('body-parser')
+
+const testRequest = {
+    template : 'my name is $name and I do $activities',
+    inputString : 'my name is Sahil and I do coding, gaming and singing',
+    variables : [
+        {
+            varName : "name",
+            type : "string"
+        },
+        {
+            varName : "activities",
+            type : "array",
+            separator : ',|and'
+        }
+    ]
+};
+const parseData = (data = testRequest) => {
+    var out = parseTextWithTemplateBase(data.inputString,data.template);
+    if(data.variables) out = ExtractArraysAndStrings(out,testRequest.variables);
+    return out;
+}
+
+express.use(bodyParser.json())
+//console.log(JSON.parse(`{"template":"my name is $name and I do $activities","inputString":"my name is Sahil and I do coding, gaming and singing","variables":[{"varName":"name","type":"string"},{"varName":"activities","type":"array","separator":",|and"}]}`));
+express.get('/',(req,res)=>res.send('GOTCHA'))
+
+express.get('/parse',(req,res)=>{
+  
+    //let data = JSON.parse(`${req.body}`);
+    let out = parseData(req.body);
+    //res.status(200).send('Cool')
+    res.json(out);
+    //console.log('get : response : ', arrayExtracted);
+})
+
+express.listen(3000,console.log(`Started Jingling on 3000`));
+
---
+++ parser.js
@@ -0,0 +1,41 @@
+function parseTextWithTemplateBase(input, template) {
+    const re = new RegExp('^' + template.replace(/\$(\w+)/g, "(?<$1>.+?)") + '$');
+    return Object.assign({}, input.match(re).groups);
+}
+
+//F'd Up... I Know.
+//Step 2
+function ExtractArraysAndStrings(input, template){
+    let out = {};
+    Object.keys(input).forEach((val)=>{
+        varTemplate = template.find((searchRes)=>searchRes.varName == val)
+        if(varTemplate){
+            switch(varTemplate.type){
+                case 'string':
+                    out = {...out , [val] : input[val] };
+                    break;
+                case 'array' :
+                    out = {...out, [val] : input[val].split(RegExp(varTemplate.separator)).map((val)=>val.trim())};
+                    break;
+            }
+        }else{
+            out = {...out, [val] : input[val]}
+        }
+    });
+    return out;
+}
+
+function insertObjectDataInString(template = "", data = {}) {
+    const regex = /\$(\w+)/g;
+    let formattedString = template;
+    while (r = regex.exec(template)) {
+        formattedString = formattedString.replace(r[0], data[r[1]]);
+    }
+    return formattedString;
+}
+
+module.exports = {
+    parseTextWithTemplateBase,
+    insertObjectDataInString,
+    ExtractArraysAndStrings
+}
