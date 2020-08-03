c05acd1120525f213fa879ab71faaf4088efc159
GC54 <sahilpohare>
2020-08-04 01:29:05 +0530

    created a testing api

---
+++ test.js
@@ -0,0 +1,25 @@
+const axios = require('axios').default;
+const fs = require('fs');
+
+var argv = process.argv.slice(2);
+
+if (argv[0].match(/^([a-zA-Z0-9\s_\\.\-\(\):])+(.json)$/)) {
+
+    console.log('File Parsed');
+
+	let rawdata = fs.readFileSync(argv[0]);
+    let testData = JSON.parse(rawdata);
+
+	axios
+		.get('http://localhost:3000/parse', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: JSON.stringify(testData)
+		})
+		.then((val) => {
+			console.log(val.data);
+        });
+} else {
+	console.log('invalid file: JSON REQUIRED');
+}
